1645fc8f8ecbf30533159ff0ae6428ca7c603819
Konrad Kubczyk <konrad@kubczyk.com>
2026-01-18 21:20:08 +0100

    Implement a prototype of the list of reminders

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
+import { ReminderCard } from "@/components/reminder-card";
+import { FlatList, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const reminders: { id: string }[] = Array.from({ length: 8 }).map(
+    (_, index) => ({
+      id: String(index),
+    }),
+  );
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView>
+      <FlatList
+        data={reminders}
+        renderItem={() => <ReminderCard />}
+        keyExtractor={(reminder) => reminder.id}
+        contentContainerStyle={styles.list}
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  list: {
+    gap: 8,
+    paddingHorizontal: 8,
+  },
+});
---
+++ components/reminder-card.tsx
@@ -0,0 +1,64 @@
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { Switch } from "@expo/ui/jetpack-compose";
+import { useState } from "react";
+
+export const ReminderCard = () => {
+  const [enabled, setEnabled] = useState(false);
+
+  return (
+    <Pressable
+      android_ripple={{
+        color: "rgba(0, 0, 0, .05)",
+        foreground: true,
+      }}
+      style={styles.card}
+    >
+      <View style={styles.detailsContainer}>
+        <Text style={styles.date}>Jan. 20 (Tue)</Text>
+        <Text style={styles.time}>14:28</Text>
+        <Text style={styles.message}>Get ready for the meeting</Text>
+      </View>
+      <View style={styles.controlsContainer}>
+        <Text style={styles.pattern}>Default</Text>
+        <TouchableWithoutFeedback>
+          <Switch value={enabled} onValueChange={setEnabled} />
+        </TouchableWithoutFeedback>
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    padding: 16,
+    backgroundColor: "#f5f5f5",
+    borderRadius: 24,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    overflow: "hidden",
+  },
+  detailsContainer: {},
+  controlsContainer: {
+    justifyContent: "space-between",
+  },
+  date: {
+    fontSize: 12,
+  },
+  time: {
+    fontSize: 24,
+  },
+  pattern: {
+    textAlign: "right",
+    fontSize: 12,
+  },
+  message: {
+    paddingTop: 8,
+    fontWeight: "semibold",
+  },
+});
